87d3e94a74549c78066139277f5db0b4c5da0326
Thomas <cornu.thomas6@gmail.com>
2025-07-22 16:26:25 +0200

    feat: création du projet Spring Boot muscul_ia

---
+++ src/main/java/com/example/muscul_ia/MusculIaApplication.java
@@ -0,0 +1,13 @@
+package com.example.muscul_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusculIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusculIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/muscul_ia/MusculIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muscul_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusculIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
